1315553c3ccfc2643f800e3758ffc766b230420e
Jack Hazlehurst <jack.hazlehurst@gmail.com>
2018-01-23 15:14:24 +1300

    shows num neighbours for each cell

--- gameOfLife/src/Simulation.java
+++ gameOfLife/src/Simulation.java
@@ -4,40 +4,42 @@ import java.awt.*;
 
 public class Simulation {
     private static Cell[][] board;
-    private static final int NUM_CELLS = 20;
+    private static final int BOARD_SIZE = 50;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         new Simulation();
     }
 
     /**
      * runs the simulation of Conway's Game of Life
      */
-    public Simulation(){
-        board = generateBoard(NUM_CELLS);
-        drawBoard(board);
+    public Simulation() {
+        board = generateBoard(BOARD_SIZE);
+        drawBoard();
     }
 
     /**
      * draws the board in its current state
-     *
-     * @param toDraw the board that is to be drawn
      */
-    private void drawBoard(Cell[][] toDraw) {
+    private void drawBoard() {
         UI.setImmediateRepaint(false);
 
         final int CELL_SIZE = 10; //size of each cell in pixels
-        final int OFFSET = 150;//offset from the borders on the top and left
+        final int OFFSET = 25;//offset from the borders on the top and left
 
-        for (int row = 0; row < toDraw.length; row++) {
-            for (int col = 0; col < toDraw[row].length; col++) {
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
                 //changes color depending on if the cell is alive or not
                 UI.setColor(Color.WHITE);
 
-                if (toDraw[row][col].isAlive())
+                if (board[row][col].isAlive())
                     UI.setColor(Color.BLACK);
 
                 UI.fillRect(OFFSET + row * CELL_SIZE, OFFSET + col * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+
+                //draws number of neighbours 
+                UI.setColor(Color.YELLOW);
+                UI.drawString(Integer.toString(numNeighbours(row, col)), OFFSET + row * CELL_SIZE, OFFSET + col * CELL_SIZE + CELL_SIZE);
             }
         }
 
@@ -53,15 +55,34 @@ public class Simulation {
     private Cell[][] generateBoard(int boardSize) {
         Cell[][] board = new Cell[boardSize][boardSize];
 
-        UI.println("Generating Board");
-
         for (int row = 0; row < boardSize; row++) {
             for (int col = 0; col < boardSize; col++) {
                 board[row][col] = new Cell();
             }
         }
 
-        UI.println("Board Generated");
         return board;
+    }
+
+    /**
+     * returns the number of neighbours for a given cell
+     * <p>
+     * Note: for cells on the edge of the board this will wrap around to the other side to look for neighbours
+     *
+     * @param row the row the cell is in
+     * @param col the column the cell is in
+     * @return the number of neighbours
+     */
+    private int numNeighbours(int row, int col) {
+        int numNeighbours = 0;
+
+        //for all neighbours check if they are alive
+        for (int rowOffSet = -1; rowOffSet <= 1; rowOffSet++)
+            for (int colOffSet = -1; colOffSet <= 1; colOffSet++)
+                if (board[Math.floorMod(row + rowOffSet, BOARD_SIZE)][Math.floorMod(col + colOffSet, BOARD_SIZE)].isAlive()
+                        && !(rowOffSet == 0 && colOffSet == 0))//so the cell does not count itself
+                    numNeighbours++;
+
+        return numNeighbours;
     }
 }
